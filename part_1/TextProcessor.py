18d37e7889286d1e1ce02eaf1cb8ec1f4472092e
lwaldner <lwaldne1@ithaca.edu>
2018-01-25 14:39:23 -0500

    finished solution for part 1

--- part_1/TextProcessor.py
+++ part_1/TextProcessor.py
@@ -1,25 +1,44 @@
-
+import requests
+from bs4 import BeautifulSoup
+from nltk import word_tokenize
+from nltk.stem import PorterStemmer
+import collections
 
 class TextProcessor:
 
     # Class constructor
     def __init__(self):
-
+        self.stemmer = PorterStemmer()
         return
 
     def fetch(self, url):
 
-        return
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "lxml")
+        for s in soup(['script', 'style']):
+            s.decompose()
+        tokens = word_tokenize(' '.join(soup.stripped_strings))
+
+        return tokens, soup.title.string
 
     def lower(self, tokens):
-
-        return
+        return list(map(str.lower, tokens))
 
     def stem(self, tokens):
-
-        return
+        return list(map(self.stemmer.stem, tokens))
 
     def getTerms(self, tokens):
+        counts = collections.Counter(tokens)
+        sortedList = sorted(tokens, key=counts.get, reverse=True)
+        return list(set(sortedList))
 
-        return
-
+t = TextProcessor()
+tokens, title= t.fetch('https://www.wired.com/2004/10/tail/')
+print(title)
+print('Number of Tokens: ' + str(len(tokens)))
+terms = t.getTerms(tokens)
+print('Number of Terms: ' + str(len(terms)))
+lowerTerms = t.getTerms(t.lower(tokens))
+print('Number of Terms after lowercase: ' + str(len(lowerTerms)))
+stemmedTokens = t.getTerms(t.stem(tokens))
+print('Number of Terms after Porter Stemmer: ' + str(len(stemmedTokens)))
